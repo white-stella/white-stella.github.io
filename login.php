a7885b28df46fe719d1351571e1111e20bbacd6d
White Stella <166137987+white-stella@users.noreply.github.com>
2024-04-06 07:21:40 +0500

    Add files via upload

---
+++ login.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://www.phptutorial.net/app/css/style.css">
+    <title>Login</title>
+</head>
+<body>
+<main>
+    <form action="login.php" method="post">
+        <h1>Login</h1>
+        <div>
+            <label for="username">Username:</label>
+            <input type="text" name="username" id="username">
+        </div>
+        <div>
+            <label for="password">Password:</label>
+            <input type="password" name="password" id="password">
+        </div>
+        <section>
+            <button type="submit">Login</button>
+            <a href="register.php">Register</a>
+        </section>
+    </form>
+</main>
+</body>
+</html>
