dc06e4ee5e6e267b0f2721b15f171c614e9088ac
White Stella <166137987+white-stella@users.noreply.github.com>
2024-09-17 18:42:29 +0500

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCuCI_G2hHPKQC4_L6Twy3Ijd_sH_R9KOU",
+  authDomain: "chatroom-101-f39ca.firebaseapp.com",
+  projectId: "chatroom-101-f39ca",
+  storageBucket: "chatroom-101-f39ca.appspot.com",
+  messagingSenderId: "154660784073",
+  appId: "1:154660784073:web:f2aaa8f3ff57cad8bb0a18",
+  measurementId: "G-MQHK73V5SF"
+};
+
+export default firebaseConfig;
